a01f0047a155a86b558c3c0991bd3b1e020f5f09
danik00111 <115283667+danik00111@users.noreply.github.com>
2023-11-20 21:05:42 +0200

    tfw when you copy code from a tutorial 1 to 1... and it DOESN'T WORK AT ALL AND YOU DON'T KNOW WHY

--- scr.js
+++ scr.js
@@ -16,46 +16,54 @@ const timeoutDisable=()=>{
   lastClickTime = Date.now();
   toggle.checked = !toggle.checked;
 };
-const gen = () => {document.getElementById('card-num').innerHTML = `${('000' + randInt(0, 9999)).slice(-4)} ${('000' + randInt(0, 9999)).slice(-4)} ${('000' + randInt(0, 9999)).slice(-4)} ${('000' + randInt(0, 9999)).slice(-4)}`;}
+const gen=()=>{
+  document.getElementById('card-num').innerHTML = `${('000' + randInt(0,9999)).slice(-4)} ${('000' + randInt(0,9999)).slice(-4)} ${('000' + randInt(0,9999)).slice(-4)} ${('000' + randInt(0,9999)).slice(-4)}`;}
 gen();
 outM.innerHTML = ('0' + ((new Date).getMonth() + 1)).slice(-2);
 outY.innerHTML = ('0' + ((new Date).getYear() + 4)).slice(-2);
 document.getElementById('date-tog').addEventListener('change', ()=>{
+  inpM.disabled=!inpM.disabled;inpY.disabled=!inpY.disabled;
   if(document.getElementById('date-tog').checked) {
-    inpM.disabled=false;inpY.disabled=false;
     outM.innerHTML=((isNaN(parseInt(inpM.value)+1))?'NA':('0'+(parseInt(inpM.value)))).slice(-2);
     outY.innerHTML=((isNaN(parseInt(inpY.value)+1))?'NA':('0'+(parseInt(inpY.value)+4))).slice(-2);
     if((inpM.value == '') || !((parseInt(inpM.value)>=1)&&(parseInt(inpM.value)<=12))) inpM.classList.add('error');
     if((inpY.value == '') || !((parseInt(inpY.value)>=yearRange[0])&&(parseInt(inpY.value)<=yearRange[1]))) inpY.classList.add('error');
   } else {
-    inpM.disabled = true; inpY.disabled = true;
     outM.innerHTML = ('0' + currentMonth).slice(-2);
     outY.innerHTML = parseInt(('0' + currentYear).slice(-2))+4;
     inpM.classList.remove('error');
     inpY.classList.remove('error');
   }
 });
+const edgeCaseCheck=()=>{
+
+}
 inpM.addEventListener('input', ()=>{
   if (!((parseInt((inpM.value))>=1)&&(parseInt((inpM.value))<=12))){inpM.classList.add('error');outM.innerHTML='ER';return;}
   inpM.classList.remove('error');
   outM.innerHTML=((isNaN(parseInt(inpM.value)+1))?'NA':('0'+(parseInt(inpM.value)))).slice(-2);
+  edgeCaseCheck();
 });
 inpY.addEventListener('input',()=>{
   if (!((parseInt((inpY.value))>=yearRange[0])&&(parseInt((inpY.value))<=yearRange[1]))){inpY.classList.add('error');outY.innerHTML='ER';return;}
   inpY.classList.remove('error');
   outY.innerHTML=((isNaN(parseInt(inpY.value)+1))?'NA':('0'+(parseInt(inpY.value)+4))).slice(-2);
+  edgeCaseCheck();
 });
 inpN.addEventListener('input',()=>{
   inpN.classList.remove('error');
+  outN.classList.remove('placeholder');
+  if(inpN.value===''){outN.classList.add('placeholder')};
   document.querySelector('notice.invalid').classList.remove('shown');
   if(!(inpN.value).match(/^[a-zA-Z]+\s[a-zA-Z]+$/)) {
     inpN.classList.add('error');
     document.querySelector('notice.invalid').classList.add('shown');
   };
-  outN.innerHTML = inpN.value;
-  // document.querySelector('notice.overflow').computedStyleMap.display = 'none';
+  outN.innerHTML = inpN.value.slice(0,26);
   document.querySelector('notice.overflow').classList.remove('shown');
+  outN.classList.remove('ellipsis');
   if(inpN.value.length>26){
+    outN.classList.add('ellipsis');
     document.querySelector('notice.overflow').classList.add('shown');
     inpN.classList.add('error');
   };
